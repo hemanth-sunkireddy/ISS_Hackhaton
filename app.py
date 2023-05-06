e9cda31618b7b8b19013e19beb2d78507c8b5687
vx3540 <127507980+vx3540@users.noreply.github.com>
2023-05-06 22:23:12 +0530

    added connections to sell

--- app.py
+++ app.py
@@ -5,24 +5,19 @@ from datetime import datetime
 app = Flask(__name__)
 
 
+
+
 def create_table():
     conn = sqlite3.connect('lfs.db')
     c = conn.cursor()
     c.execute("CREATE TABLE IF NOT EXISTS users (name TEXT, id INTEGER PRIMARY KEY AUTOINCREMENT,password TEXT, number TEXT)")
-    c.execute(
-        "CREATE TABLE IF NOT EXISTS sale (name TEXT, description TEXT, price TEXT, rollno TEXT)")
-    c.execute(
-        "CREATE TABLE IF NOT EXISTS lost (name TEXT, colourmodel TEXT, description TEXT, rollno TEXT)")
+    c.execute("CREATE TABLE IF NOT EXISTS sale (name TEXT, description TEXT, price TEXT, rollno TEXT, status TEXT)")
+    c.execute("CREATE TABLE IF NOT EXISTS lost (name TEXT, colourmodel TEXT, description TEXT, rollno TEXT)")
     conn.commit()
     conn.close()
 
-
 create_table()
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 
 @app.route('/signup', methods=['POST'])
@@ -32,11 +27,13 @@ def signup():
         student_rollno=request.form['rollNo']
         student_mobile=request.form['mobile']
         student_password=request.form['password']
-        print(student_name)
-        print(student_mobile)
-        print(student_password)
-        print(student_rollno)
-    return render_template('homepage.html')
+        conn = sqlite3.connect('lfs.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO users (name, id,password,number) VALUES (?, ?,?,?)", (student_name, student_rollno,student_mobile,student_password))
+        conn.commit()
+        conn.close()
+        render_template('homepage.html')
+    
 
 
 @app.route('/signin', methods=['POST', 'GET'])
@@ -46,7 +43,57 @@ def signin():
         student_password=request.form['password']
         print(student_password)
         print(student_rollno)
-    return render_template('homepage.html')
+        render_template('homepage.html')
+
+
+@app.route('/')
+def index():
+    return render_template('sell.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+   
+    name = request.form['name']
+    description = request.form['description']
+    price=request.form['price']
+    action=request.form['action']
+
+    conn = sqlite3.connect('lfs.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO sale (name, description, price, status) VALUES (?, ?, ?, ?)", (name, description, price, 0 if action=='Sell' else 1 if action=='Lend' else 2))
+    conn.commit()
+    conn.close()
+    return render_template('sell.html')
+
+
+
+@app.route('/submit1', methods=['POST'])
+def submit1():
+    name = request.form['name']
+    description = request.form['description']
+    colourmodel=request.form['colourmodel']
+
+    conn = sqlite3.connect('lfs.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO lost (name, description, colourmodel) VALUES (?, ?, ?)", (name, description,colourmodel))
+    conn.commit()
+    conn.close()
+    return render_template('found.html')
+
+
+@app.route('/submit2', methods=['POST'])
+def submit2():
+    name = request.form['name']
+    description = request.form['description']
+    colourmodel=request.form['colourmodel']
+
+    conn = sqlite3.connect('lfs.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO lost (name, description, colourmodel) VALUES (?, ?, ?)", (name, description,colourmodel))
+    conn.commit()
+    conn.close()
+    return render_template('lost.html')
 
 @app.route('/view.html')
 def view():
@@ -94,13 +141,5 @@ def lostView():
 @app.route('/borrow.html')
 def borrow():
     return render_template('borrow.html')
-
-
-
-
-
-
 if __name__ == '__main__':
     app.run(debug=True)
-
-
